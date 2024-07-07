ddf522b8a31dd92243742c75b075c30aeae86217
Jude Osam-Sackey <43099552+judeo-s@users.noreply.github.com>
2024-07-07 02:14:43 +0000

    had to remove some flash messages

--- app.py
+++ app.py
@@ -58,7 +58,6 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user and check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
-            flash('Logged in successfully!', 'success')
             return redirect(url_for('home'))
         else:
             flash('Login Unsuccessful. Please check email and password', 'danger')
