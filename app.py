741cddc7330875a7d8bf4dd38771a94ef61656dd
Jude Osam-Sackey <43099552+judeo-s@users.noreply.github.com>
2024-07-07 01:52:56 +0000

    had to remove the jsonify module, I was not using it

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from forms import LoginForm, RegistrationForm
 from werkzeug.security import generate_password_hash, check_password_hash
-import random, jsonify
+import random
 import concurrent.futures
 import api
 import time
